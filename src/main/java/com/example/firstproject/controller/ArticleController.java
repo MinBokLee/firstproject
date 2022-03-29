9e128480e75ed79dc040f62f3d45c7a3cbc02bb8
minboklee <lik707@gmail.com>
2022-03-29 09:17:05 +0900

    데이터 목록 조회 추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -57,6 +59,17 @@ public class ArticleController {
         // 3: 보여줄 페이지를 설정!
 
        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        // 1: 모든 Article을 가져온다!
+            List<Article> articleEntityList = articleRepository.findAll();
+        // 2: 가져온 Articles 묶음을 뷰로 전달!
+            model.addAttribute("articleList", articleEntityList);
+        // 3: 뷰 페이지를 설정!
+
+        return "articles/index"; //articles/index.mustchse
     }
 
 }
--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,9 +3,11 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 //Entity 의 대표값의 타입을 두 번째 인자로 넣어줘야 한다.
 // Article을 CRUD 하는 기본 등작을 추가코딩 및 구현없이 사용이 가능 하다.
 public interface ArticleRepository extends CrudRepository<Article,Long> {
-
-
+    @Override
+    ArrayList<Article> findAll();
 }
