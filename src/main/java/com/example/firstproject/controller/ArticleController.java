a2a4cbc096645ecad2233ef46bafda2e3850583f
minboklee <lik707@gmail.com>
2022-03-30 17:47:14 +0900

    Changes

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -7,9 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -95,5 +97,20 @@ public class ArticleController {
         }
         // 3: 수정 결과 페이지로 리다이렉트한다.
         return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제요청이 들어왔습니다.");
+        // 1: 삭제 대상을 가져온다!
+         Article target = articleRepository.findById(id).orElse(null);
+         log.info(target.toString());
+        // 2: 그 대상을 삭제 한다!
+        if(target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제가 완료되었습니다!");
+        }
+        // 3: 결과 페이지로 리다이렉트한다.
+        return "redirect:/articles";
     }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,12 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity // DB가 해당 객체를 인식 가능!
+@Entity // DB가 해당 객체를 인식 가능! (해당 클래스로 테이블을 만든다.)
 @AllArgsConstructor //lombok 기본 생성자 자동 생성!
 @ToString //lombok ToString 자동 생성!
 @NoArgsConstructor //lombok 디폴트 생성자를 추가!
@@ -20,7 +17,7 @@ public class Article {
     //기본적으로 entity엔 대표값을 하나 넣어줘야 한다.
 
     @Id //대표 값을 지정! like a 주민등록번호
-    @GeneratedValue // 1, ,2 ,3 ... 자동 생성 어노테이션!
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 id를 자동 생성 어노테이션!
     private Long id;
 
     @Column
