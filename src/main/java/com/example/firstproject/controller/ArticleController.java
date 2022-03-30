7a1a9439f30c7947800f7beae9612e86ab0afb19
minboklee <lik707@gmail.com>
2022-03-30 15:34:08 +0900

    수정 및 조회, data.sql 추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -77,4 +77,23 @@ public class ArticleController {
         // 뷰 페이지 설정
         return "articles/edit";
     }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+
+        // 1: DTO를 엔티티로 변환한다!
+        Article articleEntity= form.toEntity();
+        log.info(articleEntity.toString());
+        // 2: 엔티티를 DB로 저장한다!
+        // 2-1: DB에서 기존 데이터를 가져온다!
+         Article target= articleRepository.findById(articleEntity.getId()).orElse(null);
+
+         //2-2: 기존 데이터에 값을 갱신한다!
+        if(target != null){
+            articleRepository.save(articleEntity); //엔티티가 DB로 갱신!
+        }
+        // 3: 수정 결과 페이지로 리다이렉트한다.
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -8,10 +8,11 @@ import lombok.ToString;
 @ToString // lombok(ToString 생성)
 public class ArticleForm {
 
+    private Long id; // id필드 추가
     private String title;
     private String content;
-    
+
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }
