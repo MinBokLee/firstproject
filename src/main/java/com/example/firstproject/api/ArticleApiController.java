6d5002866d5ad609bf01d02fb09422d782641630
minboklee <lik707@gmail.com>
2022-04-01 13:02:45 +0900

    Changes HTTPS * RestAPI컨트롤러 추가 구현

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,11 +3,15 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController //RestAPI 용 컨트롤러! 데이터(JSON)을 반환
 public class ArticleApiController {
 
@@ -34,5 +38,42 @@ public class ArticleApiController {
     }
     //PATH
 
-    //DELETE
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id,
+                                          @RequestBody ArticleForm dto){
+        // 1:수정용 엔티티 생성
+             Article article = dto.toEntity();
+             log.info("id:{}, article: {}", id, article.toString());
+
+        // 2: 대상 엔티티를 조회
+            Article target= articleRepoistory.findById(id).orElse(null);
+        // 3: 잘못된 요청 처리(대상이 없거나, id가 다른 경우)
+        if(target == null || id != article.getId()){
+            //400, 잘못된 요청 응답!
+            log.info("잘못된 요청!  id:{}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        // 4: 업데이트 및 정상 응답(200)
+        target.patch(article);
+        Article updated = articleRepoistory.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+        //DELETE
+        @DeleteMapping("/api/articles/{id}")
+        public ResponseEntity<Article> delete(@PathVariable Long id){
+            //대상 찾기
+             Article target = articleRepoistory.findById(id).orElse(null);
+
+           // 잘못된 요청 처리
+           if(target == null){
+               return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+           }
+
+           //대상 삭제
+            articleRepoistory.delete(target);
+            //대상 반환
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+
+
 }
--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,13 +1,12 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @AllArgsConstructor // lombok(기본 생성자 생성)
 @NoArgsConstructor //lombok 디폴트 생성자를 추가!
 @ToString // lombok(ToString 생성)
+@Setter
 public class ArticleForm {
 
     private Long id; // id필드 추가
